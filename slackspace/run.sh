4efd02f66f4838f6239fca6c8db13e8b5d90ab4b
DeepDive9 <99256474+DeepDive9@users.noreply.github.com>
2024-07-23 12:10:28 +0200

    fix: improved run.sh

--- slackspace/run.sh
+++ slackspace/run.sh
@@ -1,1 +1,3 @@
-sudo python3 hider.py --purge && sudo python3 hider.py --target /media/usb_mount_point --input test.txt --id 1 && sudo python3 hider.py --target /media/usb_mount_point --output test2.txt --id 1
+#! /bin/bash
+echo "--- Idempotent slackspace hiding for demo purposes. Make sure you followed the setup tutorial! ---"
+sudo python3 hider.py --target /media/usb_mount_point --purge && sudo python3 hider.py --target /media/usb_mount_point --input test.txt --id 1 && sudo python3 hider.py --target /media/usb_mount_point --output test2.txt --id 1
